b794e35705b44b4ec2ee58c701150b21556b7f58
XuChunling <xucl@sari.ac.cn>
2019-12-04 10:28:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import tensorflow as tf
 flags = tf.app.flags
 flags.DEFINE_integer("epoch", 25, "Epoch to train [25]")
 flags.DEFINE_float("learning_rate", 0.0002, "Learning rate of for adam [0.0002]")
-flags.DEFINE_float("G_rate", 2, "Learning rate of for adam [0.0002]")
+flags.DEFINE_float("G_rate", 2, "generator learning rate is default twice as much as learning rate of for adam [2]")#modify by jiangdh
 flags.DEFINE_float("beta1", 0.5, "Momentum term of adam [0.5]")
 flags.DEFINE_float("train_size", np.inf, "The size of train images [np.inf]")
 flags.DEFINE_integer("batch_size", 64, "The size of batch images [64]")
